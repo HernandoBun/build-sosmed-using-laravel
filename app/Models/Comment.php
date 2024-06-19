4eec31144e18140d5cd85bf447b4d36bb5315273
HernandoBun <hernandobun@gmail.com>
2024-06-19 16:30:19 +0700

    membuat file Model Comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'tweet_id',
+        'user_id',
+        'komentar'
+    ];
+
 }
