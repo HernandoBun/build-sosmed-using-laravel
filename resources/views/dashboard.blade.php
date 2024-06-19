57ae35dae3c42317a59191aed6e32e7224f1f34b
zieksthi <louischuannata05@gmail.com>
2024-06-19 13:23:42 +0700

    fixing display untuk menampilkan tweet

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,6 +20,34 @@
                         </form>
                     </div>
 
+                    <div class="mt-4 flex flex-col space-y-2">
+                    {{-- mengambil $tweets yg dikirim oleh fungsi tampil di controller --}}
+                    @foreach ($tweets as $tweet)
+                        <div class="card bg-primary w-96 text-primary-content mb-2">
+                            
+                            <div class="card-body">
+                                {{-- nama user --}}   {{-- akses tabel user kolom name --}} 
+                                <h2 class="card-title">{{ $tweet->user->name }}</h2>
+
+                                {{-- tweet user --}}
+                                <p>{{ $tweet->postingan }}</p>
+                            </div>
+
+                            <div class="card-action p-3 ">
+                                <a href="{{ route('tweets.edit', $tweet->id) }}" class="btn btn-warning">Edit!</a>
+                                <form action="{{ route('tweets.hapus', $tweet->id) }}" method="POST" style="display:inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <input type="submit" class="btn btn-error" value="Hapus">
+                                </form>
+				<p class="text-end p-3 text-xs" style="display:inline">{{ $tweet->created_at->diffForHumans() }}</p>
+                            </div>
+
+                            
+                        </div>
+                    @endforeach
+                </div>
+
                 </div>
 
                 
