aa25521fca11edbaca607325998c2be7c31cb45d
Indryrianto <indryrianto023@gmail.com>
2024-06-20 15:13:00 +0700

    menampilkan alert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ $title ?? config('app.name') }}</title>
+        <title>{{ $title ?? config('app.name') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -14,6 +14,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
+    
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
@@ -29,6 +30,16 @@
 
             <!-- Page Content -->
             <main>
+                {{-- jika session di controller berhasil dikirmkan maka tampilkan pesan berhasil  --}}
+                @if(session('success'))
+                    <div class="alert alert-success">{{ session('success') }}</div>
+                @endif
+
+                {{-- pesan untuk session hapus tweet  --}}
+                @if(session('danger'))
+                    <div class="alert alert-error">{{ session('danger') }}</div>
+                @endif
+
                 {{ $slot }}
             </main>
         </div>
