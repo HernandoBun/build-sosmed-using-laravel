2a07f8a06200e6f44314009db26d0f2844b6c355
Darren <darrenamadeus1@gmail.com>
2024-06-21 08:42:23 +0700

    Menampilkan Comment

--- resources/views/tweets/perluas.blade.php
+++ resources/views/tweets/perluas.blade.php
@@ -39,6 +39,22 @@
 
             <input type="submit" value="Komen!" class="btn btn-outline btn-success">
         </form>
+        <div class="mt-4 flex flex-col space-y-2 p-2">
+        {{-- mengambil $tweets yg dikirim oleh fungsi tampil di controller --}}
+        @foreach ($tweet->comments as $comment)
+            <div class="card bg-primary w-96 text-primary-content mb-2">
+                
+                <div class="card-body">
+                    {{-- nama user --}}   {{-- akses tabel user kolom name --}} 
+                    <h2 class="card-title">{{ $comment->user->name }}</h2>
+
+                    {{-- tweet user --}}
+                    <p>{{ $comment->komentar }}</p>
+                </div>
+
+            </div>
+        @endforeach
+    </div>
     </div>
    
 </x-app-layout>
